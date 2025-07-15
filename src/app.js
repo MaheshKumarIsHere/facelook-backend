b976c2e8ec5d1909899125804329961ac1d925ce
mahesh <maheshkumar@gmail.com>
2025-07-15 17:24:22 +0530

    morgan setup as production and development mode and script also added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import path from "node:path";
 // 3RD PARTY MODULES
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 // APP MODULES
 
 // CONSTANTS
@@ -13,6 +14,17 @@ const app = express();
 
 // DOTENV CONFIGURATIOn
 dotenv.config({ path: DOTENV_FILE_PATH });
+
+// MIDDLEWARES
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  console.log("dev");
+  app.use(morgan("dev"));
+}
+if (process.env.NODE_ENV === "production") {
+  console.log("pro");
+  app.use(morgan("tiny"));
+}
 
 // ROUTES
 // Testing Route
